5f3d3bf0c25e32119938a967725881f7d2dcca59
elliot-star5 <matticatee@protonmail.com>
2022-03-21 22:23:54 +0530

    changed position of helper function

--- clib/features.h
+++ clib/features.h
@@ -21,32 +21,3 @@ int rq_memcmp(const char *dest, const char *src, size_t bytes, size_t rq_cmp)
         return 1;
 
 }
-
-/* returns pointer to given format string and store it in memory */
-char *make_msg(char *fmt, ...)
-{
-        char *p = 0;
-        int size = 0;
-        va_list ap;
-        
-        va_start(ap, fmt);
-        size = vsnprintf(p, size, fmt, ap);
-        va_end(ap);
-        
-        ++size;                 /* for '\0' */
-        p = malloc(size);
-        if (p == NULL)
-                return NULL;
-        
-        va_start(ap, fmt);
-        size = vsnprintf(p, size, fmt, ap);
-        va_end(ap);
-        
-        if (size < 0)           /* if vsnprintf fails */
-        {
-                free(p);
-                return NULL;
-        }
-        
-        return p;
-}
