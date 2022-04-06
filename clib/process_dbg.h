f8fec8350338a0a0a9bbd7aab242aeb8b4487336
elliot-star5 <70165559+elliot-star5@users.noreply.github.com>
2022-04-07 00:18:15 +0530

    changed indentation style

--- clib/process_dbg.h
+++ clib/process_dbg.h
@@ -1,48 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/user.h>
+#include <sys/ptrace.h>
+
+// process info struct contains information process id 
+// of child process and also holds file descriptor that
+// pipes which is used to communicate with child process
 struct process_info
 {
-  // child process id
-  pid_t child_pid;
-  int talk_fd[2];
+	// child process id
+	pid_t child_pid;
+	int talk_fd[2];
 };
 
 void executeCLD(
-    struct process_info* self,      // process info struct
-    const char *prog_name,          // name of program to be executed
-    void (*parent_exec_fcn)(struct process_info*) // parent function that should called 
-    )
+		struct process_info* self,			// process info struct
+		const char *prog_name,					// name of program to be executed
+		void (*parent_exec_fcn)(struct process_info*) // parent function that should called 
+		)
 {
-  int from_child[2], from_parent[2];
+	int from_child[2], from_parent[2];
 
-  // pipes for communicating with child
-  pipe(from_child);
-  pipe(from_parent);
+	// pipes for communicating with child
+	pipe(from_child);
+	pipe(from_parent);
 
-  // fork a child process
-  self->child_pid = fork();
-  assert(self->child_pid >= 0);
-  if (!self->child)
-  {
-    // replace stdin and stdout with pipes
-    dup2(from_parent[0], STDIN_FILENO);
-    dup2(from_child[1], STDOUT_FILENO);
+	// fork a child process
+	self->child_pid = fork();
+	assert(self->child_pid >= 0);
+	if (!self->child_pid)
+	{
+		// replace stdin and stdout with pipes
+		dup2(from_parent[0], STDIN_FILENO);
+		dup2(from_child[1], STDOUT_FILENO);
 
-    close(from_parent[0]);
-    close(from_child[1]);
+        // close unused file descriptors
+		close(from_parent[0]);
+		close(from_child[1]);
 
-    execl(prog_name, prog_name, NULL);
-  }
-  else
-  {
-    // close unused file descriptor
-    close(from_child[1]);
-    close(from_parent[0]);
+        // traced by the parent process
+        ptrace(PTRACE_TRACEME, NULL, NULL, NULL);
 
-    talk_fd[0] = from_child[0];
-    talk_fd[1] = from_parent[1];
+        // execute  program 
+		execl(prog_name, prog_name, NULL);
+	}
+	else
+	{
+		// close unused file descriptor
+		close(from_child[1]);
+		close(from_parent[0]);
 
-    parent_exec_fcn(self);
+		self->talk_fd[0] = from_child[0];
+		self->talk_fd[1] = from_parent[1];
 
-    close(from_child[0]);
-    close(from_parent[1]);
-  }
+		parent_exec_fcn(self);
+
+		close(from_child[0]);
+		close(from_parent[1]);
+	}
 }
